52bd01433c6adf054e7308ef71bdd1759ae4e2b1
Garrett Moseke <gmosedev@proton.me>
2024-12-02 17:13:41 +0100

    solve day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -8,11 +8,23 @@ pub fn part_one(input: &str) -> Option<u32> {
     for i in 0..first.len() {
         sum += first[i].abs_diff(second[i]);
     }
-    return Some(sum)
+    return Some(sum);
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (first, second) = advent_of_code::file_util::split_to_arrays(input);
+    let f = second
+        .iter()
+        .fold(std::collections::HashMap::new(), |mut dict, val| {
+            dict.entry(val).and_modify(|count| *count += 1).or_insert(1);
+            return dict;
+        });
+
+    let mut sum = 0;
+    for num in first.iter() {
+        sum += num * f.get(num).unwrap_or(&0);
+    }
+    return Some(sum);
 }
 
 #[cfg(test)]
@@ -28,6 +40,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
