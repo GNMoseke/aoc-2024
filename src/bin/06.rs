e368a56901ef599cdcffd8a7c05c6f804f32c09d
Garrett Moseke <gmosedev@proton.me>
2024-12-06 12:19:52 +0100

    solve day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,12 +1,40 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(6);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (grid, start) = parse_to_grid(input);
+    let (visited, _) = walk_grid(grid, start);
+    Some(visited.len() as u32 + 1)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    // brute force every position on the guard's path but store direction as well
+    // if we repeat direction when adding a new obstacle we've found a loop
+    let (grid, start) = parse_to_grid(input);
+    let (mut valid_visited, _) = walk_grid(grid.clone(), start);
+    let mut sum = 1;
+    valid_visited.remove(&(start.0, start.1));
+    for path_loc in valid_visited {
+        let mut new_grid = grid.clone();
+        new_grid[path_loc.0][path_loc.1] = false;
+        let (_, loop_found) = walk_grid(new_grid, start);
+        if loop_found {
+            sum += 1;
+        }
+    }
+    Some(sum)
+}
+
+// set of traversed gridpoints, flag if we hit a loop
+fn walk_grid(
+    grid: Vec<Vec<bool>>,
+    start: (usize, usize, Direction),
+) -> (HashSet<(usize, usize)>, bool) {
     let mut solved = false;
-    // NOTE: UNIQUE positions dumbass, took me way too long to find this stupid bug
-    let mut visited = HashSet::<(usize, usize)>::new();
+    let mut looped = false;
+    // store directions in here as well, if we repeat a direction on a gridpoint we've looped
+    let mut visited = HashMap::<(usize, usize), Vec<Direction>>::new();
     let mut position = start;
     while !solved {
         match position.2 {
@@ -17,8 +45,15 @@ pub fn part_one(input: &str) -> Option<u32> {
                     // specifically don't add to the sum here, just turn
                     position = (position.0, position.1, Direction::Right);
                 } else {
-                    visited.insert((position.0, position.1));
+                    let dirs_this_pos = visited
+                        .entry((position.0, position.1))
+                        .or_insert_with(|| vec![]);
+                    if dirs_this_pos.contains(&position.2) {
+                        looped = true;
+                        break;
+                    }
                     position = (position.0 - 1, position.1, position.2);
+                    dirs_this_pos.push(position.2)
                 }
             }
             Direction::Down => {
@@ -28,8 +63,15 @@ pub fn part_one(input: &str) -> Option<u32> {
                     // specifically don't add to the sum here, just turn
                     position = (position.0, position.1, Direction::Left);
                 } else {
-                    visited.insert((position.0, position.1));
+                    let dirs_this_pos = visited
+                        .entry((position.0, position.1))
+                        .or_insert_with(|| vec![]);
+                    if dirs_this_pos.contains(&position.2) {
+                        looped = true;
+                        break;
+                    }
                     position = (position.0 + 1, position.1, position.2);
+                    dirs_this_pos.push(position.2)
                 }
             }
             Direction::Left => {
@@ -39,8 +81,15 @@ pub fn part_one(input: &str) -> Option<u32> {
                     // specifically don't add to the sum here, just turn
                     position = (position.0, position.1, Direction::Up);
                 } else {
-                    visited.insert((position.0, position.1));
+                    let dirs_this_pos = visited
+                        .entry((position.0, position.1))
+                        .or_insert_with(|| vec![]);
+                    if dirs_this_pos.contains(&position.2) {
+                        looped = true;
+                        break;
+                    }
                     position = (position.0, position.1 - 1, position.2);
+                    dirs_this_pos.push(position.2)
                 }
             }
             Direction::Right => {
@@ -50,20 +99,23 @@ pub fn part_one(input: &str) -> Option<u32> {
                     // specifically don't add to the sum here, just turn
                     position = (position.0, position.1, Direction::Down);
                 } else {
-                    visited.insert((position.0, position.1));
+                    let dirs_this_pos = visited
+                        .entry((position.0, position.1))
+                        .or_insert_with(|| vec![]);
+                    if dirs_this_pos.contains(&position.2) {
+                        looped = true;
+                        break;
+                    }
                     position = (position.0, position.1 + 1, position.2);
+                    dirs_this_pos.push(position.2)
                 }
             }
         }
     }
-    Some(visited.len() as u32 + 1)
+    (visited.keys().copied().collect(), looped)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -105,6 +157,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
